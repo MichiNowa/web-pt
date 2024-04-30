2e45801f4bbc9ea954baddb4764e4e69df33a3ea
arvinjayguno <arvinguno73@gmail.com>
2024-04-30 10:52:31 +0800

    2-arvin

--- view/profile.php
+++ view/profile.php
@@ -24,6 +24,10 @@
             </div>
         </div>
         <br>
+        <hr>
+        
+        <br>
+        
         <h6>Skills</h6>
         <div class="skills">
             <ul>
@@ -70,13 +74,17 @@
                 <div class="inline">
                     <small class="float-end text-navy">1h ago</small>
                     <strong><img src="th.jpg" alt="" width="40px" class="img" style="border-radius: 100px;"> Michi Arvin Lhil</strong> Upload a photo<br />
-                    @mal
+               <br>
+                    <div class="caption">
+                    <small><p>My Games</p></small>
+                </div>
                     <figure><img src="roblox.jpg" alt="" width="400px" height="400">
                     <figcaption><small class="text-muted">Today 6:35 pm</small></figcaption>
                 </figure>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
                 <div class="interaction-buttons">
+                    <hr>
                     <button class="bi-heart" style="margin-right: 70px;" > Heart</button>
                     <button class="bi-chat" style="margin-right: 70px;"> Comment</button>
                     <button class="bi-share" style="margin-right: 70px;"> Share</button>
@@ -94,8 +102,8 @@
             <div class="activity">
                 <div class="inline">
                     <small class="float-end text-navy">1h ago</small>
-                    <strong><img src="th.jpg" alt="" width="40px" class="img" style="border-radius: 100px;"> Michi Nowa</strong> posted something on <strong>ArGunz's </strong>timeline<br />
-                    @mal
+                    <strong><img src="th.jpg" alt="" width="40px" class="img" style="border-radius: 100px;"> Michi Arvin Lhil</strong> posted something on <strong>ArGunz's </strong>timeline<br />
+                <br>
                     <div>
                         <p>Happy Birthday ArGunz!!!</p>
                     </div>
@@ -103,18 +111,86 @@
                 </div>
           
                 <div class="interaction-buttons">
+                    <hr>
+                <button class="bi-heart" style="margin-right: 70px;" > Heart</button>
+                    <button class="bi-chat" style="margin-right: 70px;"> Comment</button>
+                    <button class="bi-share" style="margin-right: 70px;"> Share</button>
+
+                    <div class="comment-area" style="border:1px solid; padding:15px; border-radius: 15px;background-color: whitesmoke;"> 
+                    
+                    <div class="comment-friend" style="background-color: whitesmoke;">
+                      <strong><img src="michi.jpg" alt="bfbf" width="35px" style="border-radius: 100px;"> Michi Nowa</strong> @michi <br>
+                       <p class="comment-p">  Happy Birthday ArGunz, ayay pakals na!  <br>  <button class="bi-heart" style="margin-right;" > </button>
+                  </p>
+                     
+                    </div>
+                   
+                    <div class="comment-friend" style="background-color: whitesmoke;">
+                      <strong> <img src="kim.jpg" alt="" width="35px" style="border-radius: 100px;"> Lhil Kim</strong>  @lhilkim <br>
+                      <p class="comment-p"> Happy Birthday ArGunz, anha ko sainyo kaon ko lechon bi gutom kaau  <br>  <button class="bi-heart" style="margin-right;" > </button></p> 
+                      <input type="text" name="comment" id="comment" class="comment" placeholder="Write  a comment" style="border-radius: 8px;">
+
+                    </div>
+                
+ 
+                </div>
+                  
+                  
+                </div>    
+            </div>
+        </div>
+        <br>
+        <hr>
+        <br>
+
+        <div class="activities">
+        
+            <div class="activity">
+                <div class="inline">
+                    <small class="float-end text-navy">1h ago</small>
+                    <strong><img src="th.jpg" alt="" width="40px" class="img" style="border-radius: 100px;"> Michi Arvin Lhil</strong> shared a photo <br />
+                    <hr>
+             
+                <strong><img src="kim.jpg" alt="" width="30px" class="img" style="border-radius: 100px;"> Lhil Kim</strong>
+          
+                <div class="caption">
+                    <small><p>My Idol</p></small>
+                </div>
+                <figure>
+                <figure><img src="image.png" alt="" width="400px" height="400">
+                <figcaption>  <small class="text-muted">Yesterday 6:35 pm</small></figcaption>
+                </figure>
+                    <div>
+                        <p></p>
+                    </div>
+                  
+                </div>
+          
+                <div class="interaction-buttons">
+                    <hr>
                 <button class="bi-heart" style="margin-right: 70px;" > Heart</button>
                     <button class="bi-chat" style="margin-right: 70px;"> Comment</button>
                     <button class="bi-share" style="margin-right: 70px;"> Share</button>
                     <div class="comment-friend" style="background-color: whitesmoke;">
-                       <p> <strong> <img src="michi.jpg" alt="" <img src="kim.jpg" alt="" width="35px" style="border-radius: 100px;" > Michi Nowa</strong> @michi <br> Happy Birthday ArGunz, ayay pakals na!</p>
-                    </div>
+                    <div class="comment-area" style="border:1px solid; padding:15px; border-radius: 15px;background-color: whitesmoke;"> 
                     
                     <div class="comment-friend" style="background-color: whitesmoke;">
-                       <p><strong> <img src="kim.jpg" alt="" width="35px" style="border-radius: 100px;"> Lhil Kim</strong>  @lhilkim <br> Happy Birthday ArGunz, anha ko sainyo kaon ko sinugba</p> 
+                      <strong><img src="michi.jpg" alt="bfbf" width="35px" style="border-radius: 100px;"> Michi Nowa</strong> @michi <br>
+                       <p class="comment-p">  Wow nice picture  <br>  <button class="bi-heart" style="margin-right;" > </button>
+                  </p>
+                     
                     </div>
-                    <br>
-                    <input type="text" name="comment" id="comment" class="comment" placeholder="Write  a comment" style="border-radius: 8px;">
+                   
+                    <div class="comment-friend" style="background-color: whitesmoke;">
+                      <strong> <img src="kim.jpg" alt="" width="35px" style="border-radius: 100px;"> Lhil Kim</strong>  @lhilkim <br>
+                      <p class="comment-p"> Luhhh kanindottt sa pictureee
+                          <br>  <button class="bi-heart" style="margin-right;" > </button></p> 
+                      <input type="text" name="comment" id="comment" class="comment" placeholder="Write  a comment" style="border-radius: 8px;">
+
+                    </div>
+                
+ 
+                </div>
                 </div>    
             </div>
         </div>
@@ -122,16 +198,17 @@
           <hr>
         <br>
         <div class="activities">
-        
+       
             <div class="activity">
                 <div class="inline">
                     <small class="float-end text-navy">1h ago</small>
-                    <strong><img src="th.jpg" alt="" width="40px" class="img" style="border-radius: 100px;"> ArGunz</strong> started  following  <strong>Michi Arvin Lhil</strong><br />
-                    @mal
+                    <strong><img src="th.jpg" alt="" width="40px" class="img" style="border-radius: 100px;"> Michi Arvin Lhil</strong> started  following  <strong>ArGunz</strong><br />
+              <br>
                     <small class="text-muted">Today 6:35 pm</small>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
                 <div class="interaction-buttons">
+                    <hr>
                 <button class="bi-heart" style="margin-right: 70px;" > Heart</button>
                     <button class="bi-chat" style="margin-right: 70px;"> Comment</button>
                     <button class="bi-share" style="margin-right: 70px;"> Share</button>
